229b077aeecd47f27e5c5704ee631cb3107afcdd
woarewe <rostislav@hey.com>
2021-06-17 00:11:22 +0300

    Decompose string and build required data structures

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -12,7 +12,36 @@ defmodule Identicon do
       :world
 
   """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+  end
+
+  defp hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+
+  defp pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, b, g}}
+  end
+
+  defp build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  defp mirror_row([first, second | _tail] = row) do
+    row ++ [second, first]
   end
 end
